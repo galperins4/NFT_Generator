7afab65641114ea468e7b3abd379fec82f3114ae
galperins4 <galperins4@gmail.com>
2021-09-09 17:18:44 -0500

    Update nftstorage.py

--- utility/nftstorage.py
+++ utility/nftstorage.py
@@ -2,17 +2,9 @@ import requests
 
 class NftStorage:
     def __init__(self, api_key):
-        #self.image = image
-        #self.metadata = metadata
         self.api_key = api_key
         self.url = 'https://api.nft.storage/upload'
         self.headers = {'Authorization': 'Bearer ' + self.api_key}
-        
-        #image_cid = self.upload_image()
-        #metadata_cid = self.upload_metadata(image_cid)
-        
-        #output = {"image_cid": image_cid, "metadata_cid": metadata_cid}
-        #return output
         
     
     def upload(self, file):
@@ -23,15 +15,3 @@ class NftStorage:
                 return response.json()['value']['cid']
         except:
             print("Something went wrong with the upload")
-      
-    '''  
-    def upload_metadata(self, image_cid):
-        data = open(self.metadata, 'rb').read()
-        try:
-            response = requests.post(self.url, headers = self.headers, data = data)
-            if response.json()['ok'] == True:
-                return response.json()['value']['cid']
-         except:
-            print("Something went wrong with the upload")
-            quit()
-    '''
