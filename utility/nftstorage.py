3e8bb7c7c3c318662dbf203eb3f5e4de161e4680
galperins4 <galperins4@gmail.com>
2021-09-11 21:37:35 -0500

    Update nftstorage.py

--- utility/nftstorage.py
+++ utility/nftstorage.py
@@ -7,6 +7,19 @@ class NftStorage:
         self.headers = {'Authorization': 'Bearer ' + self.api_key}
         
     
+    def upload(self, file_list, file_type):
+        files = []
+        for i in file_list:
+            files.append(('file':, (i, open(i, 'rb'), file_type)))
+        
+        try:
+            response = requests.post(self.url, headers = self.headers, files = files)
+            if r.json()['ok'] == True:
+                return r.json()['value']['cid']
+        except:
+            print("Something went wrong with the upload")
+            
+    '''
     def upload(self, file):
         data = open(file, 'rb').read()
         try:
@@ -15,3 +28,4 @@ class NftStorage:
                 return response.json()['value']['cid']
         except:
             print("Something went wrong with the upload")
+     '''
